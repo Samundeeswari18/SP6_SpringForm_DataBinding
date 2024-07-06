e8ecf57939806a91602fcb26bcad7656e25f7fe7
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 13:36:22 -0400

    SpringForm DataBinding

---
+++ src/main/java/com/sam/sp6_springforms_databinding/Sp6SpringFormsDataBindingApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sp6_springforms_databinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp6SpringFormsDataBindingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp6SpringFormsDataBindingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/sp6_springforms_databinding/Sp6SpringFormsDataBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sp6_springforms_databinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp6SpringFormsDataBindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
